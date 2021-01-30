f92edddca7ed24a70aec58616510ec20ad6a5c81
ljc <632849093@qq.com>
2021-01-30 15:13:50 +0800

    初始化

---
+++ src/layouts/DefaultLayout.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+export default class DefaultLayout extends Component{
+    render(){
+        return (
+            <div id="DefaultLayout">
+                HelloWorld
+            </div>
+        )
+    }
+}
